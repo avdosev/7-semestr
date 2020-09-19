7308c0bfa441f54e80e48c3368412de46d7e6b1b
Sapfir0 <sapfir999999@yandex.ru>
2020-09-19 13:40:07 +0300

    fix

--- Data protection/2/Untitled1.cpp
+++ Data protection/2/Untitled1.cpp
@@ -31,5 +31,50 @@ int main() {
 	  LocalFree(pszName);
 	}	
 	
+	
+	
+	
+	
+	
+	
+	
+	HCRYPTPROV hProv;
+	HCRYPTKEY hSessionKey;
+	
+	if (!CryptAcquireContext(&hProv, NULL, NULL, 
+	PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+	{
+			std::cout << "CryptAcquireContext" << std::endl;
+	  return 0;
+	}
+	
+	std::cout << "Cryptographic provider initialized" << std::endl;
+	
+	
+	int key0 = CryptGenKey(hProv, CALG_RC4, CRYPT_ENCRYPT | CRYPT_DECRYPT, &hSessionKey);
+	std::cout << key0 << std::endl;
+	
+	if (!CryptGenKey(hProv, CALG_RC4, CRYPT_ENCRYPT | CRYPT_DECRYPT, &hSessionKey))
+	{
+		std::cout << "CryptGenKey" << std::endl;
+	  return 0;
+	}
+	
+	std::cout << "Session key generated" << std::endl;
+	
+	char string[]="Test";
+	DWORD count=strlen(string);
+	
+	if (!CryptEncrypt(hSessionKey, 0, true, 0, (BYTE*)string, &count, strlen(string)))
+	{
+			std::cout << "CryptEncrypt" << std::endl;
+	  return 0;
+	}
+	
+	std::cout << "Encryption completed" << std::endl;
+	
+	// ???????? ????? ?? ?????
+	std::cout << "Encrypted string: " << string << std::endl;
+		
 }
 
