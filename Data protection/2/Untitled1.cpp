dec2a1c865dfde93f4657a5512cf3b894c1e921c
Sapfir0 <sapfir999999@yandex.ru>
2020-09-19 11:49:24 +0300

    Первая часть второй лабки по ЗИ

---
+++ Data protection/2/Untitled1.cpp
@@ -0,0 +1,33 @@
+//-------------------------------------------------------------------
+#include "windows.h"
+#include "Wincrypt.h"
+#include <stdio.h>
+#include <iostream>
+
+
+int main() {
+	DWORD dwIndex=0;
+	DWORD dwType;
+	DWORD cbName;
+	LPTSTR pszName;
+	while (CryptEnumProviders(dwIndex, NULL, 0, &dwType, NULL, &cbName))
+	{
+	  if (!cbName)
+	    break;
+	   
+	  if (!(pszName = (LPTSTR)LocalAlloc(LMEM_ZEROINIT, cbName)))
+	    return 0;
+	  
+	  if (!CryptEnumProviders(dwIndex++, NULL, 0, &dwType, pszName, &cbName))
+	  { 
+	    return 0;
+	  }
+	  
+	  std::cout<<"--------------------------------"<<std::endl;
+	  std::cout<<"Provider name: "<<pszName<<std::endl; 
+	  std::cout<<"Provider type: "<<dwType<<std::endl;
+	  
+	  LocalFree(pszName);
+	}	
+}
+
