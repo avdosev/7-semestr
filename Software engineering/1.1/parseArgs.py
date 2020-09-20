95e03b9be3a6c3f19372f0c3a705edf2307fc13e
Alexander Yuriev <sapfir999999@yandex.ru>
2020-09-20 12:45:18 +0400

    Доработка ПРИН

--- Software engineering/1.1/parseArgs.py
+++ Software engineering/1.1/parseArgs.py
@@ -1,26 +1,31 @@
-from timer import my_timer
+from timer import timeTest
 
 
-@my_timer
-def validateArgs(args):
-    print(args)
+def validateArgs(args, timeit, outputTimeit):
 
-    types = ['int', 'float', 'str']
+    @timeTest(timeit, outputTimeit)
+    def validateArgsInner(args):
+        print(args)
 
-    if args.type not in types:
-        raise Exception("Не распознан тип, допустимые значения: int, str, float")
+        types = ['int', 'float', 'str']
 
-    if args.type == 'int' or args.type == 'float':
-        if not args.distibution:
-            raise Exception("Не задано распределение, допустимые значения: normal, uniform")
-        if not args.mean and not args.min_value and not args.max_value and not args.std:
-            raise Exception("Не заданы параметры распределения")
+        if args.type not in types:
+            raise Exception("Не распознан тип, допустимые значения: int, str, float")
+
+        if args.type == 'int' or args.type == 'float':
+            if not args.distibution:
+                raise Exception("Не задано распределение, допустимые значения: normal, uniform")
+            if not args.mean and not args.min_value and not args.max_value and not args.std:
+                raise Exception("Не заданы параметры распределения")
+    return validateArgsInner(args)
 
 
-@my_timer
-def writeResult(args, resultedData):
-    if args.output:
-        with open(args.output, 'w') as f:
-            f.writelines(str(resultedData))
-    else:
-        print(resultedData)
+def writeResult(outputPipe, resultedData, timeit, outputTimeit):
+    @timeTest(timeit, outputTimeit)
+    def writeResultInner(outputPipe, resultedData):
+        if outputPipe:
+            with open(outputPipe, 'w') as f:
+                f.writelines(str(resultedData))
+        else:
+            print(resultedData)
+    return writeResultInner(outputPipe, resultedData)
