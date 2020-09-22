6f649c564f18714dbfaabc2cc6e1aaadf5990259
Alexander Yuriev <sapfir999999@yandex.ru>
2020-09-23 01:44:44 +0400

    Пусть будет такой normalint

--- Software engineering/1.1/randomizer.py
+++ Software engineering/1.1/randomizer.py
@@ -1,18 +1,29 @@
 import numpy as np
 from config import eachStringLength
+import random
+import scipy.stats as ss
 
 
 def getUniformInt(dataCount, minValue, maxValue):  # Return random integers from the "discrete uniform" distribution
     return np.random.randint(minValue, maxValue, dataCount)
 
-# def getNormalInt(dataCount, mean, std):
+
+def getNormalInt(dataCount, mean, std, minValue, maxValue):
+    x = np.arange(minValue, maxValue)
+    xU, xL = x + 0.5, x - 0.5
+    prob = ss.norm.cdf(xU, scale=3) - ss.norm.cdf(xL, scale=3) # Cumulative distribution function of the given RV.
+    prob = prob / prob.sum()  # normalize the probabilities so their sum is 1
+    nums = np.random.choice(x, size=dataCount, p=prob)
+    return nums
 
 
 def getNormalFloat(dataCount, mean, std):
     return np.random.normal(mean, std, dataCount)
 
+
 def getUniformFloat(dataCount, minValue, maxValue):
     return np.random.uniform(minValue, maxValue, dataCount)
+
 
 def getRandomString(dataCount, charset, symbolsCountOfEachString):
     res = []
@@ -34,7 +45,7 @@ def getRandomData(dataCount, type, distribution, minValue, maxValue, std, mean, 
         if distribution == 'uniform':
             randomNumber = getUniformInt(dataCount, minValue, maxValue)
         elif distribution == 'normal':
-            pass
+            randomNumber = getNormalInt(dataCount, mean, std, minValue, maxValue)
     elif type == 'float':
         if distribution == 'uniform':
             randomNumber = getUniformFloat(dataCount, minValue, maxValue)
