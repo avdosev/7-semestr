2e7d88df1b662ef8bf93142b51c351500e75417f
Alexander Yuriev <sapfir999999@yandex.ru>
2020-10-04 18:22:34 +0400

    Вторая лаба по мультимедиям

---
+++ Gaming technology/lab2/Assets/MyCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCube : MonoBehaviour
+{
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 100;
+        transform.RotateAround(cube.transform.position, Vector3.up, speed * Time.deltaTime);
+        
+    }
+}
--- Gaming technology/libpngc/src/main.cpp
+++ Gaming technology/libpngc/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv) {
 
-    char* src = "C:\\Users\\Sapfir\\Documents\\7\\Gaming technology\\libpngc\\data\\4.png";
+    char* src = "C:\\Users\\Sapfir\\Documents\\7\\Gaming technology\\libpngc\\data\\squares.png";
     char* dest0 = "C:\\Users\\Sapfir\\Documents\\7\\Gaming technology\\libpngc\\data\\out0.png";
     char* dest6 = "C:\\Users\\Sapfir\\Documents\\7\\Gaming technology\\libpngc\\data\\out6.png";
     char* dest9 = "C:\\Users\\Sapfir\\Documents\\7\\Gaming technology\\libpngc\\data\\out9.png";
