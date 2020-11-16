14198f10fafd0176db3d2027119bb2dd803c207d
Alexander Yuryev <sapfir999999@yandex.ru>
2020-11-16 16:38:34 +0400

    Попробовал сделать перемещние персонажа по сторонам

--- Gaming technology/subwaySurf/Assets/PlayerController.cs
+++ Gaming technology/subwaySurf/Assets/PlayerController.cs
@@ -8,49 +8,59 @@ public class PlayerController : MonoBehaviour
     /*private bool isMovingRight = false;
     private bool isMovingLeft = false;*/
     public Avatar player;
+    public GameObject player2;
     private float gravity = 20f;
     private Animator anim;
+
+    private Rigidbody rb;
     private float verticalSpeed = 500f;
-    private int line = 1;
-    // Start is called before the first frame update
+    private Lines line = Lines.middle;
+
+    enum Lines
+    {
+    left, middle, right
+    }
     void Start()
     {
         anim = GetComponent<Animator>();
+        InvokeRepeating("Move", 0f, 0.1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Move()
     {
-        if (Input.GetButtonDown("Jump"))
-        {
-            anim.SetTrigger("isJump");
-            verticalSpeed -= gravity * Time.deltaTime;
-
-            transform.position += new Vector3(0f, verticalSpeed * Time.deltaTime, 0f);
-
-            if (transform.position.y <= 0.0)
-            {
-                transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
-            }
-        }
-
         var inputHorizontal = Input.GetAxis("Horizontal");
+        Debug.Log(line);
         
         if (inputHorizontal > 0.0)
         {
-            if (line < 2)
+            if (line == Lines.left || line == Lines.middle)
             {
                 anim.SetTrigger("isMovingRight");
+                player2.transform.position += new Vector3(3f, 0.0f, 0.0f);;
+                line++;
             }
             
         }
         else if (inputHorizontal < 0.0)
         {
-            if (line > 0)
+            if (line == Lines.right || line == Lines.middle)
             {
                 anim.SetTrigger("isMovingLeft");
+                player2.transform.position += new Vector3(-3f, 0.0f, 0.0f);;
+                line--;
             }
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("isJump");
+        }
+        
 
     }
 }
